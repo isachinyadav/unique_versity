70ae560ca87fe53013fb49d10ca5e3bbef9b0a80
Sachin Yadav <sachinyadav68763@gmail.com>
2024-11-01 21:00:53 +0530

    project

---
+++ include/header.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html lang="zxx">
+
+<head>
+
+    <!--========= Required meta tags =========-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>E.visa - Immigration and Visa Consulting Template</title>
+
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="images/x-icon"/>
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <style>
+     .btn_a{
+        border: none;
+    font-size: 16px;
+    color: black;
+    
+     }
+
+     .btn_a:hover{
+        color: #0d6efd;
+     }
+    </style>
+
+
+    <!-- css include -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/odometer.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+
+<body>
+
+<!-- backtotop - start -->
+<div class="xb-backtotop">
+    <a href="#" class="scroll">
+        <i class="far fa-arrow-up"></i>
+    </a>
+</div>
+<!-- backtotop - end -->
+
+<!-- preloader start -->
+<div id="xb-loadding">
+    <div class="loader">
+        <div class="plane">
+            <img class="plane-img" src="assets/img/icon/plane.gif" alt="">
+        </div>
+        <div class="earth-wrapper">
+            <div class="earth"></div>
+        </div>
+    </div>
+</div>
+<!-- preloader end -->
+
+<div class="body_wrap">
+
+    <!-- header start -->
+    <header class="site-header header-style-one">
+        <div class="header__top-wrap gray-bg">
+            <div class="container">
+                <div class="header__top ul_li_between">
+                    <div class="header__top-cta">
+                        <img src="assets/img/icon/n_pad.svg" alt=""><span>Help Desk :</span> +91 590 088 55
+                    </div>
+                    <ul class="header__top-info ul_li">
+                        <li><img src="assets/img/icon/time.svg" alt="">Monday - Friday 09:00 am - 05:00 Pm</li>
+                        <li><img src="assets/img/icon/location.svg" alt="">456 Elm Avenue Springfield, IL 62701</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="header__wrap stricky">
+            <div class="container">
+                <div class="header__inner ul_li_between">
+                    <div class="header__logo">
+                        <a href="index.html"><img src="assets/img/logo/logo.svg" alt=""></a>
+                    </div>
+                    <div class="main-menu__wrap ul_li navbar navbar-expand-lg">
+                        <nav class="main-menu collapse navbar-collapse">
+                            <ul>
+                                <li class=""><a href="index.html"><span class="btn_a">Home</span></a>
+                                  
+                                </li>
+                                <li class="">
+                                    <a href="#!"><span class="btn_a">About</span></a>
+                                  
+                                </li>
+                                <li><a href="about.html"><span class="btn_a">Succes Story</span></a></li>
+                              
+                                <li><a href="contact.html"><span class="btn_a">Study Destination</span></a></li>
+                                <li><a href="contact.html"><span class="btn_a">Contact</span></a></li>
+
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="xb-hamburger-menu">
+                        <div class="xb-nav-mobile">
+                            <div class="xb-nav-mobile-button"><i class="fal fa-bars"></i></div>
+                        </div>
+                    </div>
+                  
+                </div>
+            </div>
+        </div>
+        <div class="xb-header-wrap">
+            <div class="xb-header-menu">
+                <div class="xb-header-menu-scroll">
+                    <div class="xb-menu-close xb-hide-xl xb-close"></div>
+                    <div class="xb-logo-mobile xb-hide-xl">
+                        <a href="index.html" rel="home"><img src="assets/img/logo/logo.svg" alt=""></a></div>
+                    
+                    <nav class="xb-header-nav">
+                        <ul class="xb-menu-primary clearfix">
+                            <li class="menu-item ">
+                                <a href="#"><span>Home</span></a>
+                               
+                            </li>
+                            <li class="menu-item ">
+                                <a href="#"><span>Pages</span></a>
+                              
+                            </li>
+                            <li class="menu-item"><a href="about.html"><span>About us</span></a></li>
+                            <li class="menu-item ">
+                                <a href="#"><span>Country</span></a>
+                               
+                            </li>
+                            <li class="menu-item ">
+                                <a href="#"><span>Blog</span></a>
+                               
+                            </li>
+                            <li class="menu-item"><a href="contact.html"><span>Contact</span></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="xb-header-menu-backdrop"></div>
+        </div>
+    </header>
+    <!-- header end -->
+
+    <!-- header search start -->
+    <div class="header-search-form-wrapper">
+        <div class="xb-search-close xb-close"></div>
+        <div class="header-search-container">
+            <form role="search" class="search-form" action="#">
+                <input type="search" class="search-field" placeholder="Search …" value="" name="s">
+            </form>
+        </div>
+    </div>
+
+
+    <div class="body-overlay"></div>
